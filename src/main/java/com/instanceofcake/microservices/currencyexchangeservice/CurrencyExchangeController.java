54205bddbeac342ae90bb251e07ab77b2c0389c2
Satish Nayak <saishkumarNayak@gmail.com>
2019-08-27 17:58:15 +0530

    Updated

--- src/main/java/com/instanceofcake/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/instanceofcake/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.instanceofcake.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	Logger logging = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	Environment env;
@@ -22,6 +26,9 @@ public class CurrencyExchangeController {
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		
 		 ExchangeValue exchangeValue = repo.findByFromCurrencyAndToCurrency(from, to);
+		 
+		 logging.info("{}",exchangeValue);
+		 
 		 exchangeValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		 return exchangeValue;
 		
